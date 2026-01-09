b9cce737464b2fb2b6ac1807dc775ea4f5482f42
Dalmanski <jayraldjohndalman@gmail.com>
2026-01-09 19:54:39 +0800

    modified:   src/resources/css/games-section.css 	modified:   src/resources/js/script.js

--- src/resources/js/script.js
+++ src/resources/js/script.js
@@ -71,8 +71,12 @@ function showSection(section) {
 
   if (section === 'games') {
     history.replaceState({}, '', '/')
+    gameListEl.classList.remove('entering')
     if (gameListEl) gameListEl.style.display = 'block'
+    void gameListEl.offsetWidth
+    gameListEl.classList.add('entering')
   } else {
+    gameListEl.classList.remove('entering')
     if (gameListEl) gameListEl.style.display = 'none'
   }
 
