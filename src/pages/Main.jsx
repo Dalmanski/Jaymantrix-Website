2ea8b0872d3347884e0ff5052004f1e5f94edefa
Dalmanski <jayraldjohndalman@gmail.com>
2026-01-10 19:11:35 +0800

    modified:   src/pages/Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -125,7 +125,7 @@ export default function Home() {
         </section>
 
         <section id="gm-rec-section" style={{ display: 'none' }}>
-          <h2>Game Record</h2>
+          <h2>GAME RECORD</h2>
           <p>You can drag the image up and down. Double click to show in full image<br />Make sure you log-in on their website to view my updated game record</p>
           <div id="gm-rec-block">    
             <h3 id="gm-rec-game-name">Unknown Game</h3>
